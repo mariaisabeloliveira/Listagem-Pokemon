4246d873539be18a12968c1bc3397be1f84a9fbf
mariaisabeloliveira <166269243+mariaisabeloliveira@users.noreply.github.com>
2024-04-07 17:04:12 -0300

    Add files via upload

---
+++ src/js/index.js
@@ -0,0 +1,24 @@
+const botaoAlterarTema = document.getElementById("botao-alterar-tema");
+
+const body = document.querySelector("body");
+
+const imagemBotaoTrocaTema = document.querySelector(".imagem-botao");
+
+
+botaoAlterarTema.addEventListener('click', () => {
+
+    const modoEscuroEstaAtivo = body.classList.contains("modo-escuro");
+
+    body.classList.toggle("modo-escuro");
+
+    if (modoEscuroEstaAtivo) {
+
+        imagemBotaoTrocaTema.setAttribute("src", "./src/imagens/sun.png");
+
+     } else {
+
+        imagemBotaoTrocaTema.setAttribute("src", "./src/imagens/moon.png");
+
+     }
+
+});
